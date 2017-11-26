93d5a0d6d81e75ab2d4854025cf2eb3819d4a3ae
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2017-11-26 17:23:23 +0100

    Initial Knex setup

--- src/database.ts
+++ src/database.ts
@@ -1,44 +1,23 @@
+/*
+-----------------------------------------------------------------------------------
+|
+| Imports
+|
+-----------------------------------------------------------------------------------
+*/
+
 import * as Knex from 'knex'
+import { Mode } from './server'
 
-export const setupKnex = () => {
-  const knexConfig = require('../../knexfile')
-  return Knex(knexConfig)
+/*
+-----------------------------------------------------------------------------------
+|
+| Setup database connection
+|
+-----------------------------------------------------------------------------------
+*/
+
+export const setupConnection = (mode: Mode) => {
+  const knexConfig = require('../knexfile')
+  return Knex(knexConfig[mode])
 }
-
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').load()
-}
-
-const path = require('path')
-
-const CONN_STRING =
-  process.env.NODE_ENV === 'testing'
-    ? process.env.DATABASE_URL_TESTING ||
-      'postgresql://localhost:5432/meal_planner_testing'
-    : process.env.DATABASE_URL || 'postgresql://localhost:5432/meal_planner'
-
-module.exports = {
-  client: 'postgresql',
-  connection: CONN_STRING,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: path.join(__dirname, '/migrations/'),
-    tableName: 'knex_migrations',
-    stub: path.join(__dirname, '/migrations/stub.js')
-  },
-  useNullAsDefault: true,
-  debug: (process.env.DEBUG || '').includes('knex')
-}
-
-const knex = Knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: '',
-    password: '',
-    database: 'my_typescript'
-  }
-})
